283a6cef55ad9ca3a6378d868c8063a5b87fb3b4
MaxBardyn <maxbardyn@gmail.com>
2020-12-11 19:31:11 +0200

    add funny banner and app.prop

---
+++ src/test/java/ul/lviv/iot/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ul.lviv.iot.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
